8c28b8ef9603379952325bcc93ed94ca7dc92f5e
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-01 20:30:30 +0000

    Number palindrome

---
+++ PythonBasics/PalindromeNumber/main.py
@@ -0,0 +1,14 @@
+class Solution(object):
+   def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        rev = 0
+        y = x
+    
+        while (y > 0):
+            rev = (rev * 10) + ( y % 10)
+            y = y // 10
+
+        return x == rev
