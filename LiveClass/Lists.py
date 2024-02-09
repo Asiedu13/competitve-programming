bb2caac52b7d72646b9345c3dbb8ef29479b0c5d
Prince Asiedu <princekofasiedu@gmail.com>
2024-02-09 03:07:31 +0000

    Notes: Class notes

---
+++ LiveClass/Lists.py
@@ -0,0 +1,28 @@
+# creating lists
+
+# Getting items (indexing)
+# negative indexing 
+
+# bitwise indexing
+
+# slicing a list
+    # negative slicing
+
+# adding to lists with append() and array[]
+
+# removing from lists with remove() and pop()
+
+# Sorting lists with sorted and sort
+
+#3 reversing with reverse
+
+# traversing a list with while, for and enumerate
+
+
+# myList = [[0]] * 5
+# myList[0][0] = 1 // explain what happens
+
+
+# Tuples
+# creating a tuple
+# packing and unpacking a tuple
